ed1fa3614beb069b7dfb567c184347706a7d695a
Greg Houle <greg.houle@shopify.com>
2017-03-02 16:35:20 -0500

    Modified Skeleton commit

--- lib/social_branding.rb
+++ lib/social_branding.rb
@@ -1,3 +1,9 @@
-module SocialBranding; end
-
 require 'social_branding/base'
+require 'social_branding/version'
+require 'social_branding/model'
+
+module SocialBranding
+  class Error < StandardError; end
+  class UnsupportedVersionError < Error; end
+  class ImportError < Error; end
+end
